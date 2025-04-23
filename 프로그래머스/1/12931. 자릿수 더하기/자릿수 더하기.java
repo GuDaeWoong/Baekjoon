6528aad93c2a92e0f7a7a1fc0d661c3dec883070
Gu DaeWoong <39939767+GuDaeWoong@users.noreply.github.com>
2025-04-23 16:43:37 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.04 ms, Memory: 71.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        String numStr = Integer.toString(n);
+        char [] numArray = numStr.toCharArray();
+        for (int num : numArray) {
+            answer += (num-'0');
+        }
+        return answer;
+    }
+}
