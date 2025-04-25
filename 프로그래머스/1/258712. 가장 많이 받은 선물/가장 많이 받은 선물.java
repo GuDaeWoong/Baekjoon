0e84264adab671d1f91269d93f6d5e52ffd426a8
Gu DaeWoong <39939767+GuDaeWoong@users.noreply.github.com>
2025-04-25 15:08:54 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 11.82 ms, Memory: 95.4 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
@@ -1,6 +1,52 @@
+import java.util.*;
+
+
 class Solution {
     public int solution(String[] friends, String[] gifts) {
-        int answer = 0;
-        return answer;
+        int n = friends.length;
+        int[][] gCounts = new int[n][n];
+        int[] score = new int[n];
+        int[] next = new int[n];
+
+        HashMap<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < friends.length; i++) {
+            map.put(friends[i], i);
+        }
+
+        for (String gift : gifts) {
+            String[] people = gift.split(" ");
+            String give = people[0];
+            String receive = people[1];
+            gCounts[map.get(give)][map.get(receive)]++;
+            score[map.get(give)]++;
+            score[map.get(receive)]--;
+        }
+//        0 0 2 0
+//        3 0 0 0
+//        1 1 0 0
+//        1 0 0 0
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int give_i = gCounts[i][j]++;
+                int give_j = gCounts[j][i]++;
+                if (give_i > give_j) {
+                    next[i]++;
+                } else if (give_i < give_j) {
+                    next[j]++;
+                } else {
+                    if (score[i] > score[j]) {
+                        next[i]++;
+                    } else if (score[j] > score[i]) {
+                        next[j]++;
+                    }
+                }
+            }
+        }
+        int max = Integer.MIN_VALUE;// 최대값을 계산해주는 곳에 최소값으로 값을 초기화
+        for (int i = 0; i < n; i++) {
+            max = Math.max(next[i], max);
+        }
+        return max;
     }
 }
+
