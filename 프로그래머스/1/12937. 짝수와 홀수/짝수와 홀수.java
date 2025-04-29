4fcfb3a5e0641d3206ccfb582c6e0a1e358aeb13
DaeWoong <39939767+GuDaeWoong@users.noreply.github.com>
2025-04-29 18:37:49 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.05 ms, Memory: 78.5 MB -BaekjoonHub

--- 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.java
+++ 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.java
@@ -1,9 +1,16 @@
+// class Solution {
+//     public String solution(int num) {
+//         if(num%2 == 0){
+//             return "Even";
+//         }else{
+//             return "Odd";
+//         }
+//     }
+// }
+
 class Solution {
     public String solution(int num) {
-        if(num%2 == 0){
-            return "Even";
-        }else{
-            return "Odd";
-        }
+        String [] oddeven = {"Even", "Odd"};
+        return oddeven[ Math.abs(num) % 2 ];
     }
 }
