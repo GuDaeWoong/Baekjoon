3c5b463434375948e44b2c1c768145ea721d545c
DaeWoong <39939767+GuDaeWoong@users.noreply.github.com>
2025-05-02 15:53:00 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 13.41 ms, Memory: 73.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -0,0 +1,46 @@
+// class Solution {
+//     public String solution(String s) {
+//         char [] arr = s.toCharArray();
+//         for (int i=1;i<=arr.length;i++) {
+//             if (i % 2 != 0) {
+//                 arr[i - 1] = Character.toUpperCase(arr[i - 1]);
+//             }
+//         }
+//         return new String(arr);
+//     }
+// }
+
+
+class Solution {
+    public String solution(String s) {
+        String[] splitList = s.split(" ");
+        String resultUpper="";
+        for (String list: splitList) {
+            for (int i=0;i<list.length();i++) {
+                if (i % 2 == 0) {
+                    resultUpper += Character.toUpperCase(list.charAt(i));
+                } else {
+                    resultUpper += Character.toLowerCase( list.charAt(i));
+                }
+            }
+        }
+        int sIndex=0;
+        int sbIndex = 0;
+        String result="";
+        while (s.length()>sIndex ) {
+            if (Character.isSpaceChar( s.charAt(sIndex))) {
+                result += String.valueOf(s.charAt(sIndex++));
+            } else if (Character.isUpperCase(resultUpper.charAt(sbIndex))) {
+                result += resultUpper.charAt(sbIndex++);
+                sIndex++;
+            } else {
+                result += resultUpper.charAt(sbIndex++);
+                sIndex++;
+            }
+        }
+        return result;
+    }
+}
+// " try hello world "
+// " Tr Y HeLlO WoRlD "
+
