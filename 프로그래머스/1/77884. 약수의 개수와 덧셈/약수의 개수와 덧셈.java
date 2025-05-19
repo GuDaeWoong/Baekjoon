004fdb836106dd1bf3cd3a7959f896b70fb550bc
DaeWoong <39939767+GuDaeWoong@users.noreply.github.com>
2025-05-19 18:02:10 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 5.04 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.java
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.java
@@ -18,3 +18,29 @@ class Solution {
         return result;
     }
 }
+
+// class Solution {
+//     public int solution(int left, int right) {
+//         int answer = 0;
+//         int[] d = {1, -1};
+        
+//         for(int i = left; i <= right; i++){
+//             int result = countDivisorsEfficient(i);
+//             answer += (i * d[result % 2]);
+//         }
+        
+//         return answer;
+//     }
+//     public int countDivisorsEfficient(int n) {
+//         int count = 0;
+//         for (int i = 1; i <= Math.sqrt(n); i++) {
+//             if (n % i == 0) {
+//                 count++;
+//                 if (i * i != n) {
+//                     count++;
+//                 }
+//             }
+//         }
+//         return count;
+//     }
+// }
